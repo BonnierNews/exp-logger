b0df27b49fcb7ad59d2eb3ccfc32a78af67bef2a
Linus Sköld <originaldecode@outlook.com>
2023-01-10 15:36:01 +0100

    some test

--- test/test.js
+++ test/test.js
@@ -15,3 +15,14 @@
   const logger = require("../index")();
   logger.info("hello world");
 }
+
+{
+  process.env.NODE_ENV = "production"
+  const logger = require("../index")({shouldPrettyPrint: true});
+  logger.info("hello world");
+}
+{
+  process.env.NODE_ENV = "test"
+  const logger = require("../index")({logLocation: "./logs/diff.log"});
+  logger.info("hello world12");
+}
