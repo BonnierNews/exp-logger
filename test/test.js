2bbb1f9cf59f79e08df18d1fa2b28d44e86dcdbb
Linus Sköld <originaldecode@outlook.com>
2023-01-11 15:55:43 +0100

    made tests clearer

--- test/test.js
+++ test/test.js
@@ -1,28 +1,27 @@
 {
-  process.env.NODE_ENV = "development"
   const logger = require("../index")();
-  logger.info("hello world");
+  logger.info("hello world 1");
 }
 
 {
-  process.env.NODE_ENV = "test"
+  process.env.NODE_ENV = "test";
   const logger = require("../index")();
-  logger.info("hello world");
+  logger.info("hello world 2");
 }
 
 {
-  process.env.NODE_ENV = "production"
+  process.env.NODE_ENV = "production";
   const logger = require("../index")();
-  logger.info("hello world");
+  logger.info("hello world 3");
 }
 
 {
-  process.env.NODE_ENV = "production"
-  const logger = require("../index")({shouldPrettyPrint: true});
-  logger.info("hello world");
+  process.env.NODE_ENV = "production";
+  const logger = require("../index")({ shouldPrettyPrint: true });
+  logger.info("hello world 4");
 }
 {
-  process.env.NODE_ENV = "test"
-  const logger = require("../index")({logLocation: "./logs/diff.log"});
-  logger.info("hello world12");
+  process.env.NODE_ENV = "test";
+  const logger = require("../index")({ logLocation: "./logs/diff.log" });
+  logger.info("hello world 5");
 }
